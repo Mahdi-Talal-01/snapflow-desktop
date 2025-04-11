dc4442834b8bf7bc81ed1ae668a9233b2bb02583
mahditalaldev <mahditalal.789@gmail.com>
2025-04-11 22:50:26 +0300

    created Auth service logic for login and register

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,13 +2,44 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class AuthService
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct() {}
+
+    public function register(Request $request)
     {
-        //
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->image_url = $request->image_url;
+        $user->save();
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        if ($token) {
+            $user = Auth::user();
+            $user->token = $token;
+            return $user;
+        }
+        return null;
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        if ($token) {
+            $user = Auth::user();
+            $user->token = $token;
+            return $user;
+        }
+        return null;
     }
 }
